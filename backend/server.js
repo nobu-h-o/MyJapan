85085ba4fb5917b60fc2cbb6ff285b443cddcb73
Nobuhiro Oto <nobuhirooto@gmail.com>
2024-11-04 20:36:07 +0900

    api.js

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,16 @@ const app = express();
 
 app.use(express.static('../frontend'));
 
-app.get('/api/config', (req, res) => {
-    res.json({ apiKey: process.env.OPENAI_API_KEY });
-  });  
+
+// Endpoint to get Google Maps API key
+app.get('/api/google-maps-config', (req, res) => {
+  res.json({ apiKey: process.env.MAPS_API_KEY });
+});
+
+// Endpoint to get OpenAI API key
+app.get('/api/openai-config', (req, res) => {
+  res.json({ apiKey: process.env.OPENAI_API_KEY });
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
